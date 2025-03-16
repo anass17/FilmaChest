0b6d53c2539611b12699e75fb35fb210774b02fa
anass17 <anassboutaib2018@gmail.com>
2025-03-16 15:14:00 +0000

    Completed functionality: Display statistics and recent added movies in Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function index() {
-        return Inertia::render('Dashboard');
+        $users_count = User::count();
+        $movies_count = Movie::count();
+
+        $recent_added_movies = Movie::select('id', 'title', 'thumbnail')->orderBy('created_at', 'desc')->limit(3)->get();
+
+        return Inertia::render('Dashboard', ['stats' => ['users' => $users_count, 'movies' => $movies_count], 'recentAdded' => $recent_added_movies]);
     }
 }
