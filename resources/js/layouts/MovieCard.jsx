9a07a7c82ba9290ef3971894432d890344445d78
anass17 <anassboutaib2018@gmail.com>
2025-03-17 20:29:46 +0000

    Added favorite button to movie cards

--- resources/js/layouts/MovieCard.jsx
+++ resources/js/layouts/MovieCard.jsx
@@ -5,12 +5,24 @@ export default function MovieCard({ movie }) {
 
     let [hours, minutes] = movie.duration.split(':');
 
+    function addToFavorite(e) {
+        e.preventDefault()
+
+        let target = e.target.closest('.favorite').classList.toggle('added-favorite');
+    }
+
     return (
         <Link href={"/movies/" + movie.id} className="group">
             <div className="relative flex flex-col min-h-64 bg-gray-800 shadow-sm border border-gray-600 rounded-lg w-full overflow-hidden bg-center bg-cover">
                 <div className="w-full h-full top-0 left-0 absolute bg-center bg-cover group-hover:scale-125 transition" style={{ backgroundImage: `url('/storage/uploads/${movie.thumbnail}')` }}></div>
                 <div className="w-full h-full top-0 left-0 absolute bg-gradient-to-t from-black to-transparent "></div>
                 <div className="p-4">
+                    <button type="button" onClick={addToFavorite} className="favorite absolute top-3 right-3 fill-white group-hover:opacity-100 transition opacity-0">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 fill-inherit" viewBox="0 0 512 512">
+                            {/* <!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--> */}
+                            <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+                        </svg>
+                    </button>
                     <div className="z-10 relative inline-block mb-4 rounded bg-[#fc684d] py-1 font-semibold tracking-wider px-4 border border-transparent text-xs text-white transition-all shadow-sm text-center">
                         {movie.category.name.toUpperCase()}
                     </div>
