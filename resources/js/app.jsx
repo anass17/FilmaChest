29d001a819563d7f72b87a93f88eba0bf0aa6c11
anass17 <anassboutaib2018@gmail.com>
2025-03-17 23:49:52 +0000

    Removed Provider Component

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,18 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { InertiaApp } from '@inertiajs/inertia-react';
 import { InertiaProgress } from '@inertiajs/progress';
-import { Provider } from 'react-redux';
-import store from './store';
 
 const app = document.getElementById('app');
 
 createRoot(app).render(
-  <Provider store={store}>
     <InertiaApp
         initialPage={JSON.parse(app.dataset.page)}
         resolveComponent={name => import(`./Pages/${name}`).then(module => module.default)}
     />
-  </Provider>
 );
 
 InertiaProgress.init({
